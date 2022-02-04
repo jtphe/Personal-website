f44ec0ae8a6c947c98ba74fec1a5ba0e4c58ed96
Jean Phe <jean.phe.fr@gmail.com>
2022-02-04 19:41:11 +0100

    fix(nav-menu): nav menu works with responsive

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,7 +2,6 @@ window.addEventListener('load', function () {
     let mainNav = document.getElementsByClassName('navbar-menu');
     let navBarToggle = document.getElementById('js-navbar-toggle');
 
-    console.log('mainNav', mainNav);
     navBarToggle.addEventListener('click', function () {
         mainNav.style.setProperty('display', 'initial');
     });
--- src/php/contact.php
+++ src/php/contact.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
     <!-- Primary Meta Tags -->
@@ -19,24 +21,20 @@
     <meta property="twitter:description" content="Vous pouvez me contacter sur cette page">
     <meta property="twitter:image" content="https://jtphe.ddns.net/public/images/profilePicture.png">
 
-    <link rel="shortcut icon" href="/favicon/favicon.png" type="image/x-icon">
-    <link rel="icon" href="/favicon/favicon.png" type="image/x-icon">
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,maximum-scale=1,user-scalable=no">
-    <meta http-equiv="content-language" content="en">
-    <meta http-equiv="content-language" content="fr">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-TileImage" content="../../public/images/favicons/ms-icon-144x144.png">
     <meta name="msapplication-TileColor" content="#3080e8">
     <meta name="msapplication-TileImage" content="../../public/images/favicons/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
-    <meta name="msapplication-TileColor" content="#000000">
-    <meta name="msapplication-TileImage" content="../../public/images/favicons/ms-icon-144x144.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
     <link rel="apple-touch-icon" sizes="152x152" href="../../public/images/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../../public/images/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../../public/images/favicons/favicon-16x16.png">
     <link rel="manifest" href="../../public/images/favicons/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3080e8">
-    <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
-    <script src="https://kit.fontawesome.com/921fec88a5.js" crossorigin="anonymous"></script>
-    <title>Contact</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script type="text/javascript">
         var _paq = window._paq || [];
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
@@ -57,18 +55,15 @@
         })();
     </script>
 </head>
-
-<html>
-
 <body>
     <?php include('../html/header.html'); ?>
     <div class="container-contact">
         <div class="contact-text">
             <div class="contact-title">
-                <h2>Une question, une demande, n'hésitez pas à me contacter 😊</h2>
+            <h1 class='section-banner-about--title'>Une question, une demande, n'hésitez pas à me contacter ! 😎</h1>
             </div>
             <div class="contact-typeform">
-                <a class="typeform-share button" href="https://form.typeform.com/c/LaNZQi" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#457B9D;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:18px;line-height:45px;text-align:center;margin:0;height:45px;padding:0px 30px;border-radius:22px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Contactez moi ! </a>
+                <a class="typeform-share button" href="https://form.typeform.com/c/LaNZQi" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#457B9D;color:white;cursor:pointer;font-family: Futura Now Headline Regular, Helvetica,Arial,sans-serif;font-size:18px;line-height:45px;text-align:center;margin:0;height:45px;padding:0px 30px;border-radius:22px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Contactez moi ! </a>
                 <script>
                     (function() {
                         var qs, js, q, s, d = document,
@@ -92,5 +87,4 @@
     </div>
     <div class="special-footer"><?php include('../html/footer.html'); ?></div>
 </body>
-
 </html>
